df7c0b40eb992da72efc2b946f73994fa836d6d0
Luis Martinez <s_k_ap3@hotmail.com>
2021-08-26 01:21:49 -0500

    primera version del servidor completa, recibe un mensaje del cliente

--- server/server.c
+++ server/server.c
@@ -7,17 +7,24 @@
 #include <string.h>
 #include <unistd.h>
 
+#define MAXLINES 1024
+
 int main(int argc, char*argv[]){
     if(argc == 2){
-        int fd;
+        int socket_server, socket_client;
+        int size_dgram;
         int puerto = atoi(argv[1]);
+        char buffer[MAXLINES];
 
         struct sockaddr_in server;
+        struct sockaddr_in client;
         server.sin_family = PF_INET;
         server.sin_port = htons(puerto);
         server.sin_addr.s_addr = INADDR_ANY;
 
-        if((fd = socket(PF_INET,SOCK_DGRAM,IPPROTO_UDP)) < 0){
+        socket_client = sizeof(socket_client);
+
+        if( (socket_server = socket(PF_INET,SOCK_DGRAM,IPPROTO_UDP)) < 0){
             perror("Error al crear el socket\n");
             exit(-1);
         }
@@ -25,13 +32,17 @@ int main(int argc, char*argv[]){
             printf("socket inizializado\n");
         }
 
-        if(bind(fd, (struct sockaddr *)&server,sizeof(struct sockaddr)) < 0){
+        if(bind (socket_server, (struct sockaddr *)&server, sizeof(struct sockaddr)) < 0){
             printf("socket creado\n");
         }else{
             printf("Error en bind\n");
             exit(-1);
         }
-        close(fd);
+
+        size_dgram = recvfrom(socket_server,(char *)buffer, MAXLINES, MSG_WAITALL, (struct sockaddr *)&client,&socket_client);
+        // buffer[size_dgram] = "\n";
+        printf("mensaje del cliente: %s\n",buffer);
+        close (socket_server);
         printf("bye\n");
     }
     else{
