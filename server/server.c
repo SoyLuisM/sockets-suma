222b4014d5523eefe769c7652aa185b6b73ced0c
Luis Martinez <s_k_ap3@hotmail.com>
2021-08-26 00:18:38 -0500

    socket del servidor creado correctamente

--- server/server.c
+++ server/server.c
@@ -8,6 +8,35 @@
 #include <unistd.h>
 
 int main(int argc, char*argv[]){
-    printf("hola soy el servidor\n");
+    if(argc == 2){
+        int fd;
+        int puerto = atoi(argv[1]);
+
+        struct sockaddr_in server;
+        server.sin_family = PF_INET;
+        server.sin_port = htons(puerto);
+        server.sin_addr.s_addr = INADDR_ANY;
+
+        if((fd = socket(PF_INET,SOCK_DGRAM,IPPROTO_UDP)) < 0){
+            perror("Error al crear el socket\n");
+            exit(-1);
+        }
+        else{
+            printf("socket inizializado\n");
+        }
+
+        if(bind(fd, (struct sockaddr *)&server,sizeof(struct sockaddr)) < 0){
+            printf("socket creado\n");
+        }else{
+            printf("Error en bind\n");
+            exit(-1);
+        }
+        close(fd);
+        printf("bye\n");
+    }
+    else{
+        printf("sintaxis del script\n");
+        printf("./Server [puerto] \n");
+    }
     return 0;
 }
